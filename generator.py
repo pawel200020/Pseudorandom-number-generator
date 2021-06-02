3d2880f2f9948a4c206e2d74f83c10ca61c1ed7c
pawel200020 <38167430+pawel200020@users.noreply.github.com>
2021-06-03 01:55:15 +0200

    generator appears

--- generator.py
+++ generator.py
@@ -1,3 +1,15 @@
 
-if __name__ == '__main__':
-    print("hello!")
+class generator:
+
+    def __init__(self,seed):
+        self.value = seed
+        self.a = 747796405
+        self.m = 4294967296
+  
+    def generateRandom(self):
+        self.value = (self.a*self.value)%self.m
+        return self.value
+
+object = generator(2)
+for i in range (1000):
+    print(object.generateRandom())

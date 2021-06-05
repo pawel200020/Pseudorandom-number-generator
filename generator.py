67d7b2fb656807465c47fd3702ed770a81af2fe3
pawel200020 <38167430+pawel200020@users.noreply.github.com>
2021-06-05 20:15:43 +0200

    new presentation

--- generator.py
+++ generator.py
@@ -1,15 +1,28 @@
 
+import numpy as np
+from matplotlib import pyplot as plt
+import array as arr
+
+
 class generator:
 
-    def __init__(self,seed):
+    def __init__(self, seed):
         self.value = seed
         self.a = 747796405
         self.m = 4294967296
-  
+
     def generateRandom(self):
-        self.value = (self.a*self.value)%self.m
+        self.value = (self.a*self.value) % self.m
         return self.value
 
+
+a = arr.array('d')
 object = generator(2)
-for i in range (1000):
-    print(object.generateRandom())
+for i in range(100000):
+    a.append(object.generateRandom())
+
+for i in range(10000):
+    print(a[i])
+
+plt.hist(a, bins=10000)
+plt.show()
